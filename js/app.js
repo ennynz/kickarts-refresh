ab405846f85cf485b74bf9feccc070958229b8fb
Enny Benzonelli <enny.nz@gmail.com>
2015-08-15 21:36:27 +1200

    adds avoid scroll on map, 3 pics, starts building mobile menu

--- js/app.js
+++ js/app.js
@@ -7,7 +7,23 @@ $(document).ready(function() {
     $('.active').removeClass('active');
     $(this).addClass('active');
   });
+  //matchHeight
   $(function() {
     $('.box-height').matchHeight();
+  });
+  // mobile menu
+  $('.menu-button, ul a').click(function(){
+    $('ul').toggleClass('show-menu');
+    $('.menu-button').toggleClass('menu-open');
+    $('.menu-button').toggleClass('menu-close');
+  });
+
+// avoids scroll-capture for map
+  $(function() {
+      $('.contact-box').click(function() {
+          $(this).find('iframe').css('pointer-events', 'all');
+      }).mouseleave(function() {
+          $(this).find('iframe').css('pointer-events', 'none');
+      });
   });
 });
