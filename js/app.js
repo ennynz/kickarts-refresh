60e484bfbdd8d94dd25445c5821c980b14554938
Enny Benzonelli <enny.nz@gmail.com>
2015-08-19 22:48:58 +1200

    creates search box

--- js/app.js
+++ js/app.js
@@ -46,4 +46,57 @@ $(document).ready(function() {
   $(function() {
     $('#includeNav').load('nav.html');
   });
+  //dropdown
+  function DropDown(el) {
+  this.dd = el;
+  this.placeholder = this.dd.children('span');
+  this.opts = this.dd.find('ul.dropdown > li');
+  this.val = '';
+  this.index = -1;
+  this.initEvents();
+  }
+  DropDown.prototype = {
+    initEvents: function() {
+      var obj = this;
+
+      obj.dd.on('click', function(event) {
+        $(this).toggleClass('active');
+        return false;
+      });
+
+      obj.opts.on('click', function() {
+        var opt = $(this);
+        obj.val = opt.text();
+        obj.index = opt.index();
+        obj.placeholder.text(obj.val);
+      });
+    },
+    getValue: function() {
+      return this.val;
+    },
+    getIndex: function() {
+      return this.index;
+    }
+  }
+
+  $(function() {
+
+    var dd = new DropDown($('#dd'));
+    var dd2 = new DropDown($('#dd2'));
+    var dd3 = new DropDown($('#dd3'));
+    var dd4 = new DropDown($('#dd4'));
+    $(document).click(function() {
+      // all dropdowns
+      $('.wrapper-dropdown-1').removeClass('active');
+    });
+
+  });
+  //search form
+  $('.name').on('blur', function(){
+    if( $(this).val().length > 0 ){
+      $(this).next().addClass('stay');
+    } else {
+      $(this).next().removeClass('stay');
+    }
+  });
 });
