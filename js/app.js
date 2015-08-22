5f85c25950a80fdca800e79399578c044ac5c43e
Enny Benzonelli <enny.nz@gmail.com>
2015-08-22 18:40:06 +1200

    simplyfies search form

--- js/app.js
+++ js/app.js
@@ -82,9 +82,6 @@ $(document).ready(function() {
   $(function() {
 
     var dd = new DropDown($('#dd'));
-    var dd2 = new DropDown($('#dd2'));
-    var dd3 = new DropDown($('#dd3'));
-    var dd4 = new DropDown($('#dd4'));
     $(document).click(function() {
       // all dropdowns
       $('.wrapper-dropdown-1').removeClass('active');
